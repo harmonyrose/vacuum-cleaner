1b701d5c55fa61b0cc042b19cbc8c646309312a8
hpeura <hpeura@mail.umw.edu>
2023-09-06 23:10:28 -0400

    Got the vacuum moving, sucking, and keeping track of how many steps have been run.

--- hpeura_vacuum.py
+++ hpeura_vacuum.py
@@ -2,10 +2,20 @@ from vacuum import VacuumAgent
 
 class HpeuraVacuumAgent(VacuumAgent):
 
-    def __init__(self):
+    def __init__(self, step = 0):
         super().__init__()
         # any initialization you want to do here
+        self.step = step
 
     def program(self, percept):
         # your amazing AI vacuum cleaner code goes here
+        self.step += 1
+        if percept[0] == 'Dirty':
+            return 'Suck'
+        
+        if percept[1] == 'None':
+            return 'Left'
+        if percept[1] == 'Bump':
+            return 'Right'
         return 'NoOp'
+    
