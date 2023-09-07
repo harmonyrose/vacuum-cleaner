87ecb643de21a4f9838d49a3012adbfd16b68568
hpeura <hpeura@mail.umw.edu>
2023-09-06 23:34:58 -0400

    Vacuum cleaner now goes in random directions

--- hpeura_vacuum.py
+++ hpeura_vacuum.py
@@ -1,21 +1,23 @@
 from vacuum import VacuumAgent
+import random
 
 class HpeuraVacuumAgent(VacuumAgent):
 
-    def __init__(self, step = 0):
+    def __init__(self, step = 0, curr_dir = "Right"):
         super().__init__()
         # any initialization you want to do here
         self.step = step
+        self.curr_dir = curr_dir
 
     def program(self, percept):
         # your amazing AI vacuum cleaner code goes here
         self.step += 1
         if percept[0] == 'Dirty':
             return 'Suck'
-        
-        if percept[1] == 'None':
-            return 'Left'
-        if percept[1] == 'Bump':
-            return 'Right'
-        return 'NoOp'
+
+
+        dirs = ['Up', 'Down', 'Right', 'Left']
+        self.curr_dir = random.choice(dirs)
+        return self.curr_dir        
+
     
