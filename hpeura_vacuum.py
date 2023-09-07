42f3e3df984c16dceee49118481569d984570cd9
hpeura <hpeura@mail.umw.edu>
2023-09-07 18:04:30 -0400

    Added support for keeping track of past directions, bumps, and turns

--- hpeura_vacuum.py
+++ hpeura_vacuum.py
@@ -3,21 +3,73 @@ import random
 
 class HpeuraVacuumAgent(VacuumAgent):
 
-    def __init__(self, step = 0, curr_dir = "Right"):
+    def __init__(self):
         super().__init__()
-        # any initialization you want to do here
-        self.step = step
-        self.curr_dir = curr_dir
+        self.step = 0
+        self.dirs = []
+        self.bumps = []
+        self.turns = []
 
     def program(self, percept):
-        # your amazing AI vacuum cleaner code goes here
+
         self.step += 1
+
         if percept[0] == 'Dirty':
             return 'Suck'
+        
+        elif self.dirs == []:
+            dir = 'Right'
+            self.log(dir, 'None', 'Straight')
+            return dir
+        
+        
+        elif percept[1] == 'Bump':
+            
+            # If this is the first time encountering a bump, turn left
+
+            dir = self.turnLeft(self.dirs[-1])
+            self.log(dir, 'Bump', 'Left')
+            return dir
 
 
-        dirs = ['Up', 'Down', 'Right', 'Left']
-        self.curr_dir = random.choice(dirs)
-        return self.curr_dir        
+        
+        elif percept[1] == 'None':
+
+            if self.turns[-1] == 'Left':
+                dir = self.turnRight(self.dirs[-1])
+                self.log(dir, 'None', 'Right')
+                return dir
+            
+            else:
+                dir = self.dirs[-1]
+                self.log(dir, 'None', 'Right')
+                return dir
+
+        
+    def turnLeft(self, lastdir):
+        if lastdir == 'Left':
+            return 'Down'       
+        elif lastdir == 'Right':
+            return 'Up'
+        elif lastdir == 'Up':
+            return 'Left'
+        else:
+            return 'Right'
+            
+    def turnRight(self, lastdir):
+        if lastdir == 'Left':
+            return 'Up'       
+        elif lastdir == 'Right':
+            return 'Down'
+        elif lastdir == 'Up':
+            return 'Right'
+        else:
+            return 'Left'
+     
+    def log(self, dir, bump, turn):
+        self.dirs.append(dir)
+        self.bumps.append(bump)
+        self.turns.append(turn)
+
 
     
